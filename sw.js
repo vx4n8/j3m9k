b299300441ab64669123e365f5b17bec85b83611
Patrice Roblin App <roblinpatrice@gmail.com>
2026-03-24 15:46:24 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,7 @@
-const CACHE = 'recce-survey-v2';
-const ASSETS = [
-  '/Tech-Survey/',
-  '/Tech-Survey/index.html',
-  '/Tech-Survey/manifest.json',
-  '/Tech-Survey/sw.js'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
+self.addEventListener('install', function() { self.skipWaiting(); });
+self.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then(function(keys) {
+    return Promise.all(keys.map(function(k) { return caches.delete(k); }));
+  }));
   self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(cached => {
-      if (cached) return cached;
-      return fetch(e.request).then(response => {
-        if (response && response.status === 200) {
-          const clone = response.clone();
-          caches.open(CACHE).then(cache => cache.put(e.request, clone));
-        }
-        return response;
-      }).catch(() => caches.match('/Tech-Survey/index.html'));
-    })
-  );
 });
